876b8ec95ea7be46b8f2d13016e73844d8e24614
Sneguradik <rrtr845@gmail.com>
2025-04-21 18:58:48 +0300

    Changed dates

--- UnusualBackend/Controllers/FilterController.cs
+++ UnusualBackend/Controllers/FilterController.cs
@@ -14,7 +14,7 @@ namespace UnusualBackend.Controllers
         [HttpGet("sort")]
         public async Task<List<TradeStatsDto>> GetData( CancellationToken token)
         {
-            var dto = new GetTradeResultDto(DateTime.Parse("2025-04-01").ToUniversalTime(), DateTime.Parse("2025-04-30").ToUniversalTime(), "RUB", [
+            var dto = new GetTradeResultDto(DateTime.Parse("2025-04-01").ToUniversalTime(), DateTime.Parse("2025-04-02").ToUniversalTime(), "RUB", [
                 "S02","S04","R0105","R0105_02","SD198","AF205","28T6S","3LLA1","AL223","AL223_002","4V430","PO256","TS267","alrbm","rencm","TS287","derzm_MM","derzm_SEP_MM","KU234_003","skbmm", "mfbim"]);
             return await tradeRepository.GetTradeResults(dto).ToListAsync(cancellationToken: token);
         }
